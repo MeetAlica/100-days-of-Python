f926c3d1fa7dbb9c3df6ac2aa4217b2c789ab042
MeetAlica <feher.ali.96@gmail.com>
2024-07-21 19:37:15 +0200

    Escape the maze

---
+++ Day 6 - Escaping the maze/main.py
@@ -0,0 +1,22 @@
+# I did this task on the reeborg.ca map called maze.
+# https://reeborg.ca/index_en.html
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+    
+# while front_is_clear():
+#     move()
+# turn_left()
+    
+# while not at_goal():
+#     if wall_on_right() and front_is_clear():
+#         move()
+#     elif wall_on_right() and wall_in_front():
+#         turn_left()
+#     elif front_is_clear():
+#         move()
+#     else:
+#         turn_right()
+#         move()
